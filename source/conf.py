19a494bb9d244f7ab46104945b6eb48249d919f4
Iqbal Abdullah <iqbal@xoxzo.com>
2019-01-03 07:41:56 +0900

    Change name to keep in line with what's on GitHub

--- source/conf.py
+++ source/conf.py
@@ -19,7 +19,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'The Studi My Project'
+project = 'The StudiMY Project'
 copyright = '2018, MARIMORE ENGINEERING SDN. BHD. (925539-H)'
 author = 'Chee Yim, Goh and Iqbal Abdullah'
 
@@ -128,7 +128,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, '101Readme.tex', 'Introduction to the Studi My Project',
+    (master_doc, '101Readme.tex', 'Introduction to the StudiMY Project',
      'Chee Yim, Goh \\and Iqbal Abdullah', 'manual'),
 ]
 
@@ -138,7 +138,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, '101readme', 'Introduction to the Studi My Project',
+    (master_doc, '101readme', 'Introduction to the StudiMY Project',
      [author], 1)
 ]
 
@@ -149,7 +149,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, '101Readme', 'Introduction to the Studi My Project',
+    (master_doc, '101Readme', 'Introduction to the StudiMY Project',
      author, '101Readme', 'One line description of project.',
      'Miscellaneous'),
 ]
